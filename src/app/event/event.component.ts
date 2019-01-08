be5021b8de618a0ed607cc9c5c14246fa63f2133
Mark Mangahas <mo.mangahas@yahoo.com>
2019-01-07 20:03:14 -0800

    working on api

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { TicketMasterApi } from '../services/ticketmaster.service';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+  styleUrls: ['./event.component.css'],
+  providers: [TicketMasterApi]
 })
-export class EventComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class EventComponent {
+  data: any[] = null;
+  constructor(private ticketMaster: TicketMasterApi) { }
+  getCityEvent(keyword: string, city: string) {
+    this.ticketMaster.getCityEvent(keyword, city).subscribe(response => {
+        this.data = response.json();
+    });
   }
-
 }
---
+++ src/app/services/ticketmaster.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class TicketMasterApi {
+    constructor(private http: Http) { }
+
+
+getCityEvent(keyword: string, city: string) {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&city=${city}&&apikey=uwC2fceKfAhcBxGIzBUXk1kZvK9QPFrJ`);
+    }
+}
